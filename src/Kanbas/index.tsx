47851a637ecb5aa38aa8b8c8c7a6db22cf4cb1df
Keyan Shen <shen.key@northeastern.edu>
2024-07-16 02:40:33 -0400

    message

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,23 +2,18 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
+import "./styles.css";
 export default function Kanbas() {
     return (
       <div id="wd-kanbas">
-        <table>
-          <tr>
-            <td valign="top">
-              <KanbasNavigation />
-            </td>
-            <td valign="top">
+          <KanbasNavigation />
+          <div className="wd-main-content-offset p-3">
             <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Dashboard" element={<Dashboard />} />
-            <Route path="Courses/:id/*" element={<Courses />} />
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Dashboard" element={<Dashboard />} />
+              <Route path="Courses/:id/*" element={<Courses />} />
             </Routes>
-            </td>
-          </tr>
-        </table>
+          </div>
       </div>
   );}
   
