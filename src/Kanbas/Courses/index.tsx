5887958fa3c085d3af4b32e8c6dbf19ef00cf800
Keyan Shen <shen.key@northeastern.edu>
2024-07-22 23:22:57 -0400

    A3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,16 +1,21 @@
+import { courses } from "../Database";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
       <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234</h2>
+      {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
       <CoursesNavigation />
       <div>
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -10,22 +10,11 @@ export default function TOC() {
       </li>
       <li className="nav-item">
         <a id="wd-a1" href="#/Labs/Lab1"
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
-          Lab 1
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a2" href="#/Labs/Lab2"
-          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
-          Lab 2
-        </a>
-      </li>
-      <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab3"
-          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
-          Lab 3
-        </a>
-      </li>
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a></li>
+          <li className="nav-item"><a id="wd-a2" href="#/Labs/Lab2"
+              className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
+          <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
+              className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
           Kanbas
