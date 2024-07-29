33f93bfb24cf03f270e3f256ae7ffc10c1aca79e
Keyan Shen <shen.key@northeastern.edu>
2024-07-29 15:53:09 -0400

    message

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,27 @@
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+export default function Lab4() {
+    function sayHello() {
+      alert("Hello");
+    }
+  
+    return(
+        <div id="wd-a4">
+            <h2>Lab 4</h2>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+        </div>
+    )
+}
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -15,6 +15,8 @@ export default function TOC() {
               className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</a></li>
           <li className="nav-item"><a id="wd-a3" href="#/Labs/Lab3"
               className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</a></li>
+          <li className="nav-item"><a id="wd-a4" href="#/Labs/Lab4"
+              className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>Lab 4</a></li>
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
           Kanbas
